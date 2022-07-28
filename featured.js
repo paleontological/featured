e0dcde4f3138e16c3efc23846f53e1f4d0e02cf5
Dave Fass <davefass@users.noreply.github.com>
2022-07-28 18:26:12 -0400

    roll back style update

--- featured.js
+++ featured.js
@@ -1,6 +1,3 @@
 $("#featured-content").load(
   "https://paleontological.github.io/featured/featured.html #featured-content > *"
 );
-$("#featured-style").load(
-  "https://paleontological.github.io/featured/featured-style.html #featured-style > *"
-);

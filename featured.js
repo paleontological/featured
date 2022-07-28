b8e23e8b8c2b777cba92c034b0c36f25f6b87896
Dave Fass <davefass@users.noreply.github.com>
2022-07-28 17:59:15 -0400

    update featured.js to stream css into style tag

--- featured.js
+++ featured.js
@@ -1,1 +1,2 @@
 $("#featured-content").load("https://paleontological.github.io/featured/featured.html #featured-content > *");
+$("#featured-style").load("https://paleontological.github.io/featured/featured.css #featured-style > *");

f110e2344ef618d3bd073c3e767cef13e15bfb75
Dave Fass <davefass@users.noreply.github.com>
2022-07-28 18:23:03 -0400

    add featured-style.html and update featured.js to pull that

--- featured.js
+++ featured.js
@@ -1,1 +1,6 @@
-$("#featured-content").load("https://paleontological.github.io/featured/featured.html #featured-content > *");
+$("#featured-content").load(
+  "https://paleontological.github.io/featured/featured.html #featured-content > *"
+);
+$("#featured-style").load(
+  "https://paleontological.github.io/featured/featured-style.html #featured-style > *"
+);
